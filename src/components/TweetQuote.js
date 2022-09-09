bebd20344e77d6cab1230991099fb180444fe9f3
Yılmaz <yilmazcite@gmail.com>
2022-09-09 19:49:14 +0300

    change tweet text

--- src/components/TweetQuote.js
+++ src/components/TweetQuote.js
@@ -4,8 +4,9 @@ import TwitterIcon from "../assets/TwitterIcon.png";
 import "../styles/RandomQuote.css";
 
 const TweetQuote = () => {
+  const websiteUrl = "— Powered by: bit.ly/3x91i2g";
   const randomTweet = useSelector((state) => state.randomQuote.randomQuote);
-  const extraTweetText = `Nursima once said: "${randomTweet}"`;
+  const extraTweetText = `Nursima once said: "${randomTweet}" ${websiteUrl}`;
   const formTweetLink = `https://twitter.com/intent/tweet?url= ${encodeURIComponent(
     extraTweetText
   )}`;
